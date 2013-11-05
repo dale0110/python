369f1adc3573e2716dbd0e70c032e5d012178baa
dale0110 <dale0110@aliyun.com>
2013-11-05 20:32:36 +0800

    Update download_jpg.py

--- download_jpg.py
+++ download_jpg.py
@@ -1,6 +1,7 @@
 import urllib
 import urllib2
 import time
+import re
 
 def get_jpg(url,path):
     socket = urllib2.urlopen(url)
@@ -8,19 +9,30 @@ def get_jpg(url,path):
     with open(path, "wb") as jpg:
         jpg.write(data)
     socket.close()
+    
+def test1():
+
+    html='''MountainMist_ZH-CN11102136834_1366x768.jpg',id:'bgDiv'''
+    #url_re = re.compile(r'''{url:\w*,id''')
+    #url_re = re.compile(r'''{url:[a-zA-Z0-9_///*-]*,id''')
+    url_re = re.compile(r'''[a-zA-Z0-9-x_]*.jpg''')
+    for  pic_url in url_re.findall(html):
+        print pic_url
+
 
 def main():
+    
     url = 'http://www.bing.com'
     f = urllib.urlopen(url)
     html = f.read()
+    #html='''{url:'http://s.cn.bing.net/az/hprichbg/rb/MountainMist_ZH-CN11102136834_1366x768.jpg',id:'bgDiv'''
+    url_re = re.compile(r'''{url:'[:.a-zA-Z0-9-x_/]*.jpg''')
+    url_list = url_re.findall(html)
+    print url_list
+    for pic_url in url_list:
+        print pic_url[6:]
+        urllib.urlretrieve(pic_url[6:],"test.jpg")
     f.close()
-    a = html[html.index('//fd//hpk2'):]
-    data = a[:a.index('/',id:')]
-    url = data.replace('//', '')
-    url = 'http://www.bing.com'+url
-    name=time.strftime("%Y%m%d", time.localtime())
-    name=name+".jpg"
-    urllib.urlretrieve(url,name)
 
 if __name__ == "__main__":
     main()
