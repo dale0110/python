a3f81600f8a7faa99825162a5baac5c9cc8e2ddb
dale0110 <dale0110@aliyun.com>
2013-11-05 22:49:56 +0800

    Update download_jpg.py

--- download_jpg.py
+++ download_jpg.py
@@ -46,17 +46,23 @@ def download_cl_pic(url):
     #src='http://102.imagebam.com/download/tT8uQQNEFqwdl5H_kDew7A/28622/286211832/460%281%29.jpg' 
     url_re = re.compile(r'''src='[%:.a-zA-Z0-9-x_/]*.jpg''')
     url_list = url_re.findall(html)
-    print url_list
+    #print url_list
     for pic_url in url_list:
-        print pic_url[6:]
-        urllib.urlretrieve(pic_url[6:],"test.jpg")
+        print pic_url[5:]
+        name=time.strftime("%Y%m%d-%H%M%S", time.localtime())
+        name=name+".jpg"
+        urllib.urlretrieve(pic_url[5:],name)
+        time.sleep(1)
     f.close()
     
 
 def main():
+    '''
     if len(sys.argv) < 2:
         usage()
-    download_cl_pic(sys.argv[1])
+    '''
+    #download_cl_pic(sys.argv[1])
+    download_cl_pic("http://cl.or.gs/htm_data/16/1311/975087.html")
 
 if __name__ == "__main__":
     main()
