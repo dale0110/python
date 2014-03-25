4d27f617f9b76c9733c816c1744d5f41d4a2d5ce
dale0110 <dale0110@aliyun.com>
2014-03-25 08:21:13 +0800

    Update txt2pic.py

--- txt2pic.py
+++ txt2pic.py
@@ -13,113 +13,95 @@ def zh_count(string):
     pattern = re.compile(re_zh)
     return len(pattern.findall(string))
 
-'''计算中文标点符号'''                          
+'''计算中文汉字数量'''                          
 def char_count(string):
-    re_char = "[\uFB00-\uFFFD]"
+    re_char = '[a-zA-Z][a-zA-Z0-9_.,;!?，。？；：]'
     pattern = re.compile(re_char)
     return len(pattern.findall(string))
+                               
+
+'''初始化全局变量'''
+#字体大小
+font_size=24
+
+#每行文本高度
+char_height=int(font_size*1.2)
+char_length=font_size*1
+
+#图片宽度
+pic_length=600
+#图片长度
+pic_height=0
+
+line_max=0
+colum_max=int(pic_length/char_length)
 
 
-def get_max(txt_file):
-    src_file = open(txt_file,'r')
-    line=0
-    for line_buf in src_file:
-        txt=line_buf.decode('gbk')
-        colum_length=zh_count(txt)+char_count(line_buf)
-        global colum_max,line_max,pic_length,pic_height,char_length,char_height
-        #print "%d %d %d %d" %(line,colum_max,zh_count(txt),char_count(txt))
-        if colum_length>colum_max:
-            colum_max=colum_length
-        line+=1
-    line_max=line
-    #图片宽度
-    #pic_length=colum_max*char_length
-    #图片长度
-    pic_height=line_max*char_height
-    src_file.close()                            
+def draw_moreline(x,y,text,font,line):
+    decode_text=text.decode('utf-8')
+    colum_length=zh_count(decode_text)+char_count(decode_text)
+    global colum_max,line_max,pic_length,pic_height,char_length,char_height
+    #print decode_text
+    print "txt_length:%d colum_length/colum_max:%d" %(len(text),int(colum_length/colum_max))
+    txt_num=int(colum_length/colum_max)+1
+    print "txt_num:%d colum_length%d" %(txt_num,colum_length)
+    i=1
+    while i<=txt_num:
+        line_buf=decode_text[i*colum_length:(i)*colum_length]
+        #print line_buf.encode('utf-8')
+        draw_oneline(x,line,line_buf,font)
+        i+=1
+        line+=i
+    return line
 
 
-class picure:
-    #字体大小
-    font_size=20
-    #每行文本高度
-    char_height=int(font_size*1.2)
-    char_length=font_size*1
-    #图片宽度
-    pic_length=800
-    #图片长度
-    pic_height=char_height
-    line_max=0
-    colum_max=0
-    x=0
-    y=0
-    '''写一行文本'''
-    def draw_line(self,text):
-        draw.text((self.x,self.y),text,(0,0,0),self.font)
-    '''将一个汉字横向打印一列'''
-    def draw_line2(self,text):
-        colum = 0
-        while colum<self.colum_max :
-            print "colum:%d"  %colum 
-            draw.text((self.x+self.font_size*colum,self.y),unicode(text,'utf-8'),(0,0,0),self.font)
-            colum+=1
 
+'''写一行文本'''
+def draw_oneline(x,y,text,font):
+    draw.text((x,y),text,(0,0,0),font)
 
-class text_line:
-    #字体大小
-    font_size=20
-    #每行文本高度
-    height=0
-    length=0
-    #图片宽度
-    pic_length=800
-    colum_max=0
-    x=0
-    y=0
-    text=''
+def draw_line2(x,y,text,font):
+    colum = 0
+    while colum<colum_max :
+        print "colum:%d"  %colum 
+        draw.text((x+font_size*colum,y),unicode(text,'utf-8'),(0,0,0),font)
+        colum+=1
     
-    def __init__(self,string,positon_x,positon_y,font_size):
-        self.font_size
-        self.text=string
-        self.x=positon_x
-        self.y=positon_y
-        self.height=int(font_size*1.2)
-        self.length=font_size*1
-    '''写一行文本'''
-    def draw_line(self):
-        draw.text((self.x,self.y),self.text,(0,0,0),self.font)
-
 
 def draw_text(txt_file,font):
     src_file = open(txt_file,'r')
     line=0
     for line_buf in src_file:
         #print line_buf.encode('utf-8')
-        #print line_buf.decode('gbk') 
-        draw_line(0,char_height*line,line_buf.decode('gbk'),font)
-        line+=1
+        line=draw_moreline(0,char_height*line,line_buf,font,line)
     src_file.close()
 
-
+def get_max(txt_file):
+    src_file = open(txt_file,'r')
+    line=0
+    for line_buf in src_file:
+        txt=line_buf.decode('utf-8')
+        #print txt
+        colum_length=zh_count(txt)+char_count(txt)
+        global colum_max,line_max,pic_length,pic_height,char_length,char_height
+        #print "%d %d %d %d" %(line_max,colum_max,pic_length,pic_height)
+        line+=int(colum_max%colum_length)+1
+    line_max=line
+    #图片长度
+    pic_height=line_max*char_height
+    src_file.close()
 
 #def main():
     
 get_max("test.txt")
-print "%f %f %f %f" %(line_max,colum_max,pic_length,pic_height)
+print "line_max:%f colum_max:%f pic_length:%f pic_height:%f" %(line_max,colum_max,pic_length,pic_height)
 
 '''图片基本信息'''
 font = ImageFont.truetype('simsun.ttc',font_size)
 img = Image.new('RGB',(pic_length,pic_height),(255,255,255))
 draw = ImageDraw.Draw(img)
 
-'''
-line = 0
-while line<line_max :
-    print "line:%d"  %line 
-    #draw_line(0,line_height*line,"你好")
-    #draw_line2(0,char_height*line,"你",font)
-    line+=1
-'''
+
 draw_text("test.txt",font)
 
  
