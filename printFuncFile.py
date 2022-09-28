731b2a521738f9dcd5b771b1c7ee18a39a4dce8b
dale0110 <4265024+dale0110@users.noreply.github.com>
2022-09-28 11:10:19 +0800

    Update printFuncFile.py

--- printFuncFile.py
+++ printFuncFile.py
@@ -3,9 +3,8 @@ import sys
 
 def test():
     #print(f"file := {__file__}"+"line := {sys._getframe().f_lineno}"+"func :={__name__}")
-    print(f"file :=", {__file__},"line :=",{sys._getframe().f_lineno},"func :=",{__name__})
-
-    print({__file__} ,{sys._getframe().f_lineno},{__name__})
+    #print(f"file :=", {__file__},"line :=",{sys._getframe().f_lineno},"func :=",{__name__})
+    print(__file__ ,sys._getframe().f_lineno,sys._getframe().f_code.co_name)
 
 
 if __name__ == '__main__':
